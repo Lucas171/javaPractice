bc372d28869031d5aec4620586c70a55ca0145b0
Lucas Miller <lucasmiller401@gmail.com>
2024-03-16 12:04:23 -0400

    update variables

--- main.java
+++ main.java
@@ -1,5 +1,11 @@
 class Main{
+//global variables
+  static int age = 28;
+
   public static void main(String[] args){
+    // prints out to the console
     System.out.println("Hello World");
+    System.out.println(age);
+
   }
 }
